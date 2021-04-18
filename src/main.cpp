1706fee9fbb54aa93156384576333fe923f9f38a
Jeff Cochran <koenigcochran@gmail.com>
2021-04-18 13:43:06 -0400

    init

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(10.f);
+    shape.setFillColor(sf::Color::Green);
+    sf::Clock clock; // starts the clock
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    sf::Time elapsed1 = clock.getElapsedTime();
+    std::cout << elapsed1.asSeconds() << std::endl;
+
+    return 0;
+}
